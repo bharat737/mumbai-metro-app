862a8f39a1025cfdf94ecc90a10374a4944e87b9
bharat737 <bharatp737@gmail.com>
2025-06-09 13:09:39 +0530

    hardocde field to dynamic

--- src/app/component/station-input/station-input.component.ts
+++ src/app/component/station-input/station-input.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { MIN_SEARCH_LENGTH } from 'src/app/constant/app.constants';
 
 @Component({
   selector: 'app-station-input',
@@ -18,15 +19,23 @@ export class StationInputComponent {
   endSuggestions: string[] = [];
 
   onStartInputChange(): void {
+     if (this.start.length >= MIN_SEARCH_LENGTH) {
     this.startSuggestions = this.stations.filter(st =>
       st.toLowerCase().includes(this.start.toLowerCase())
     );
+  }else{
+     this.startSuggestions = [];
+  }
   }
 
   onEndInputChange(): void {
+    if (this.end.length >= MIN_SEARCH_LENGTH) {
     this.endSuggestions = this.stations.filter(st =>
       st.toLowerCase().includes(this.end.toLowerCase())
     );
+  }else{
+     this.endSuggestions = [];  
+  }
   }
 
   selectStartStation(station: string): void {
---
+++ src/app/constant/app.constants.ts
@@ -0,0 +1,5 @@
+export const MIN_SEARCH_LENGTH = 4;
+
+export const AC = 'AC';
+
+export const AP = 'AP';
